49a4cacb6f745e4d9a2508622328b54d7907cbde
jiuyuehe <xm.xiaom@gmail.com>
2013-08-20 18:27:03 +0800

    webstromide
    ide

--- models/mongodb.js
+++ models/mongodb.js
@@ -2,5 +2,5 @@ var settings = require('../settings'),
     Db = require('mongodb').Db,
     Connection = require('mongodb').Connection,
     Server = require('mongodb').Server;
-   // console.log("setting.host"+settings.host);
+    console.log("setting.host"+settings.host);
 module.exports = new Db(settings.db, new Server(settings.host, Connection.DEFAULT_PORT, {}), {safe: true});
